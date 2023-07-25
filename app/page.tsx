6e866558597263bfaa3f9ea2139340031d5161b7
caoboyeehaw <dylanhoangcao@gmail.com>
2023-07-25 00:56:01 -0500

    added auth frontend page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,36 @@
+// app/pages/Home.tsx
+// use client
+
+import { useState } from 'react';
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { ModeToggle } from '@/components/ui/mode-toggle'
-import { UserAuthForm } from "@/components/ui/user-auth-form"
+import AuthenticationModal from "@/components/ui/authenticationmodal"
 
 export default function Home() {
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    function openModal() {
+        setModalOpen(true);
+    }
+
+    function closeModal() {
+        setModalOpen(false);
+    }
+
     return (
-        
       <div className="flex justify-between items-center px-4 py-4 w-full md:w-full lg:w-3/4 xl:w-2/3 mx-auto">
-        <Image src="/public/sample-labs-logo.png" alt="Logo" width="50" height="50" className="" />
-        <h1 className="text-xl ">Sample Labs</h1>
-        <div className="flex space-x-4">
-          <Button variant="secondary">Log in</Button>
-          <Button variant="primary">Sign Up</Button>
-          <ModeToggle></ModeToggle>
+        <div className="flex items-center space-x-2">
+          <Image src="/sample-labs-logo.png" alt="Logo" width={40} height={40} />
+          <h1 className="text-xl font-semibold">Sample Labs</h1>
         </div>
+        <div className="flex space-x-4">
+        <ModeToggle></ModeToggle>
+          <Button variant="secondary" size="sm">Log in</Button>
+          <Button variant="primary" size="sm" onClick={openModal}>Sign Up</Button>
+        </div>
+
+        <AuthenticationModal isOpen={isModalOpen} onRequestClose={closeModal} />
       </div>
     )
-  }
-  
-  
+}
