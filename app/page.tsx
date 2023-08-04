6f2b6ea4440aeb6d78f498f165bcbe25e0c74a90
caoboyeehaw <dylanhoangcao@gmail.com>
2023-08-04 05:28:49 -0500

    yeah

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ const Home: React.FC = () => {
 
           <Link href="/about" passHref>
             <div className="flex items-center space-x-1 cursor-pointer">
-              <Image src="/sample-labs-logo.png" alt="Logo" width={35} height={35} />
+
               <span className="text-xl font-medium">WAVLABS</span>
               <span className="text-xs mb-1 font-medium bg-primary rounded text-primary-foreground px-1">ALPHA</span>
             </div>
