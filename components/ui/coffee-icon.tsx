94958e08d1913f9109cb471b17dc7d95fccabc1f
caoboyeehaw <dylanhoangcao@gmail.com>
2023-08-05 09:18:39 -0500

    buy coffee

--- components/ui/coffee-icon.tsx
+++ components/ui/coffee-icon.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button-newyork"
 import { Icons } from "@/components/icons"
 
 export function CoffeeIconLink() {
-  const repoUrl = "https://www.buymeacoffee.com/caoboyeehaw";
+  const repoUrl = "https://ko-fi.com/caoboyeehaw";
 
   return (
     <Button asChild variant="ghost" size="icon">
