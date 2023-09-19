5dc7a8b5639f1f3b14e06a61d4648fe39dcaa057
Philodo <70660848+caoboyeehaw@users.noreply.github.com>
2023-09-19 15:36:44 -0500

    huge changes

---
+++ app/navbar.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { Button } from '@/components/ui/button-newyork';
+import { ButtonWavLabs } from '@/components/ui/button-wavlabs';
+import { GithubIconLink } from '@/components/ui/github-icon';
+import { CoffeeIconLink } from '@/components/ui/coffee-icon';
+import { ModeToggle } from '@/components/ui/mode-toggle';
+import { Icons } from "@/components/icons"
+import AuthenticationModal from "@/components/ui/authenticationmodal";
+
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const openModal = () => {
+      setIsOpen(true);
+    };
+  
+    const closeModal = () => {
+      setIsOpen(false);
+    };
+
+
+  return (
+    <div className="relative w-full mt-2">
+      <div className="border-b border-accent">
+        <div className="2xl:container relative flex justify-between items-center mx-auto backdrop-blur mb-2">
+            <div className="flex items-center space-x-4">
+                <Link href="/about" passHref>
+                    <div className="flex items-center space-x-1 cursor-pointer pl-4">
+                    <Image src="/wavlabs-logo.png" alt="Logo" width={25} height={25} />
+                    <span className="text-2xl font-semibold">WavLabs</span>
+                    <span className="text-xl font-normal text-zinc-400">| cloud</span>
+                    </div>
+                </Link>
+            </div>
+
+            <div className="flex items-center border rounded-full">
+            <input 
+                type="text" 
+                placeholder="Search..." 
+                className="p-2 rounded-l-full w-60"  // w-60 sets the width to 15rem
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
+
+              <button className="p-2 rounded-r-full">
+                <Icons.search/>
+              </button>
+            </div>
+
+
+            <div className="flex space-x-3 z-20 mt-1 pr-4">
+                <p className="flex space-x-3">
+                    <ButtonWavLabs className="text-sm font-bold" variant="ghost2" size="sm" onClick={openModal}>+ Upload</ButtonWavLabs>
+                    <ButtonWavLabs className="text-sm font-bold" variant="primary" size="sm" onClick={openModal}>Log in </ButtonWavLabs>
+                </p>
+
+            </div>
+        </div>
+      </div>
+      {isOpen && <AuthenticationModal isOpen={isOpen} onRequestClose={closeModal} />}
+    </div>
+    );
+}
--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,22 +1,48 @@
+import React, { useState } from 'react';
+import Navbar from '@/app/navbar';
+
+import Link from 'next/link';
+import Image from 'next/image'
+
+import { Button } from '@/components/ui/button-newyork'
 import { ButtonWavLabs } from '@/components/ui/button-wavlabs'
+import { GithubIconLink } from '@/components/ui/github-icon'
+import { CoffeeIconLink } from '@/components/ui/coffee-icon'
+import { ModeToggle } from '@/components/ui/mode-toggle'
+
+import AuthenticationModal from "@/components/ui/authenticationmodal"
 
 export default function Sidebar() {
     return (
-      <div>
+      <div className="pl-4 space-y-2">
+
         <p className="flex items-center space-x-1 ">
-          <ButtonWavLabs className="text-md font-normal" variant="ghost" size="sm">Explore</ButtonWavLabs>
+          <ButtonWavLabs className="text-xl font-semibold" variant="ghost" size="sm">Explore</ButtonWavLabs>
         </p>
   
         <p className="flex items-center space-x-1">
-          <ButtonWavLabs className="text-md font-normal" variant="ghost" size="sm">Popular</ButtonWavLabs>
+          <ButtonWavLabs className="text-xl font-semibold" variant="ghost" size="sm">Popular</ButtonWavLabs>
         </p>
   
         <p className="flex items-center space-x-1">
-          <ButtonWavLabs className="text-md font-normal" variant="ghost" size="sm">Ranking</ButtonWavLabs>
+          <ButtonWavLabs className="text-xl font-semibold" variant="ghost" size="sm">Ranking</ButtonWavLabs>
         </p>
   
         <p className="flex items-center space-x-1">
-          <ButtonWavLabs className="text-md font-normal" variant="ghost" size="sm">Threads</ButtonWavLabs>
+          <ButtonWavLabs className="text-xl font-semibold" variant="ghost" size="sm">Threads</ButtonWavLabs>
+        </p>
+
+        <p className="flex">
+          <GithubIconLink />
+          <CoffeeIconLink />
+          <ModeToggle />
+        </p>
+
+        <p className="text-xs font-normal">
+          About Newsroom Contact Careers
+          TikTok for Good Advertise Developers Transparency TikTok Rewards TikTok Embeds
+          Help Safety Terms Privacy Creator Portal Community Guidelines
+          @ Wavlabs 2023
         </p>
       </div>
     );
